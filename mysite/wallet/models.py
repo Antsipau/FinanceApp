00ce2ab9376dd495b26384071b7a23bef30e6ac7
Antsipau <antsipau@tut.by>
2022-06-30 12:24:31 +0300

    add models

--- mysite/wallet/models.py
+++ mysite/wallet/models.py
@@ -1,17 +1,18 @@
-from datetime import date
 from django.db import models
 
 
 class Income(models.Model):
     """This model describes incomes"""
     date_of_income = models.DateField(auto_now_add=True)
-    amount_of_income = models.DecimalField(..., max_digits=10, decimal_places=2)
+    amount_of_income = models.DecimalField(max_digits=10, decimal_places=2)
+    total_income = models.DecimalField(max_digits=20, decimal_places=2)
 
 
 class PurchasedGoods(models.Model):
     """This model describes list of purchased goods"""
     date_of_purchase = models.DateField(auto_now_add=True)
     nameof_product = models.CharField(max_length=255)
-    price_per_item = models.DecimalField(..., max_digits=10, decimal_places=2)
+    price_per_item = models.DecimalField(max_digits=10, decimal_places=2)
     quantity_of_goods = models.IntegerField()
+    amount_of_expenses = models.DecimalField(max_digits=20, decimal_places=2)
 

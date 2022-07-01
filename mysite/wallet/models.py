52ec0d036d0c64699b0efa44b9a7bfdf87bbb950
Antsipau <antsipau@tut.by>
2022-07-01 15:16:11 +0300

    add options

--- mysite/wallet/models.py
+++ mysite/wallet/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Income(models.Model):
     """This model describes incomes"""
-    date_of_income = models.DateField(auto_now_add=True, verbose_name='Date')
+    date_of_income = models.DateField(auto_now_add=True, db_index=True, verbose_name='Date')
     amount_of_income = models.DecimalField(max_digits=10, decimal_places=2, null=True, verbose_name='Amount of income')
 
     def __str__(self):
@@ -20,7 +20,7 @@ class TotalIncome(models.Model):
 
 class PurchasedGoods(models.Model):
     """This model describes list of purchased goods"""
-    date_of_purchase = models.DateField(auto_now_add=True, verbose_name='Date')
+    date_of_purchase = models.DateField(auto_now_add=True, db_index=True, verbose_name='Date')
     name_of_product = models.CharField(max_length=255, null=True, verbose_name='Name of product')
     price_per_item = models.DecimalField(max_digits=10, decimal_places=2, null=True, verbose_name='Price per item')
     quantity_of_goods = models.IntegerField(null=True, verbose_name='Quantity of goods')

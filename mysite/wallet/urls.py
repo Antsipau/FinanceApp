33885ee58a76cc4a04b6983bb9c7b6fc0b316594
Antsipau <antsipau@tut.by>
2022-07-01 15:48:46 +0300

    add income page

--- mysite/wallet/urls.py
+++ mysite/wallet/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('', main_page),
     path('test_page/', test),
+    path('income_page', income),
 
 ]
--- mysite/wallet/views.py
+++ mysite/wallet/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Income
 
 def main_page(request):
-    return HttpResponse("It will be main page")
+    return HttpResponse("It will be home page")
+
+def income(request):
+    my_income = Income.objects.all()
+    res = '<h1>My incomes</h1>'
+    for i in my_income:
+        res += f'<div>\n<p>{i.date_of_income}</p>\n<p>{i.amount_of_income}</p>\n</div>\n<hr>\n'
+    return HttpResponse(res)
 
 
 def test(request):

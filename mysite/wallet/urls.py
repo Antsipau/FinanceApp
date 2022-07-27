4518def61a1e31a0c10bcded313fb9b95094659c
Antsipau <antsipau@tut.by>
2022-07-27 15:10:50 +0300

    add urls names

--- mysite/wallet/urls.py
+++ mysite/wallet/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', main_page),
-    path('income_page/', income),
-    path('total_income_page/', my_total_income),
-    path('purchased_goods_page/', my_purchased_goods),
+    path('', main_page, name='home'),
+    path('income_page/', income, name='my_income'),
+    path('total_income_page/', my_total_income, name='total_income_page'),
+    path('purchased_goods_page/', my_purchased_goods, name='my_purchased_goods'),
 
 ]

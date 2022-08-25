7cdb9aff8c2cb450f6967903450a91e03af1c8c9
Antsipau <antsipau@tut.by>
2022-08-25 12:24:22 +0300

    edit base page

--- mysite/wallet/urls.py
+++ mysite/wallet/urls.py
@@ -5,7 +5,6 @@ from .views import *
 urlpatterns = [
     path('', main_page, name='home'),
     path('income_page/', income, name='my_income'),
-    path('total_income_page/', my_total_income, name='total_income_page'),
     path('purchased_goods_page/', my_purchased_goods, name='my_purchased_goods'),
     path('add_income_page/', add_income, name='add_income'),
     path('add_purchase_page/', add_purchase, name='add_purchase'),

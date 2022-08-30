8e7f963f11a791482f2c7f449d5d03360e519f6c
Antsipau <antsipau@tut.by>
2022-08-30 15:54:52 +0300

    edit forms

--- mysite/wallet/forms.py
+++ mysite/wallet/forms.py
@@ -5,7 +5,7 @@ class IncomeForm(forms.Form):
     """Form that consists of 2 fields: Type of income, Amount of income User must fill in these fields"""
     type_of_income = forms.CharField(max_length=255, required=False,
                                      widget=forms.TextInput(attrs={"class": "form-control"}))
-    amount_of_income = forms.DecimalField(max_digits=10, decimal_places=2,
+    amount_of_income = forms.DecimalField(max_digits=10, decimal_places=2, min_value=0.1,
                                           widget=forms.NumberInput(attrs={"class": "form-control"}))
 
 
@@ -13,6 +13,6 @@ class PurchaseForm(forms.Form):
     """Form """
     name_of_product = forms.CharField(max_length=255, required=True,
                                       widget=forms.TextInput(attrs={"class": "form-control"}))
-    price_per_item = forms.DecimalField(max_digits=10, decimal_places=2,
+    price_per_item = forms.DecimalField(max_digits=10, decimal_places=2, min_value=0.1,
                                         widget=forms.NumberInput(attrs={"class": "form-control"}))
     quantity_of_goods = forms.IntegerField(min_value=1)
